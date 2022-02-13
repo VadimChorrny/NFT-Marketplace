9232be66135428283385a73c0d0aed1377d4d6da
Vadim Chorrny <chorrny228@gmail.com>
2022-02-13 06:55:52 +0200

    add loger to cart controller

--- AbiturientStatistic/Controllers/CartController.cs
+++ AbiturientStatistic/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using DAL.Entity;
 using DAL.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,12 +14,15 @@ namespace API.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
-        private readonly ICartService _cartService;
-        public CartController(ICartService cartService)
+        public readonly ICartService _cartService;
+        public readonly ILogger<CartController> _logger;
+        public CartController(ICartService cartService, ILogger<CartController> logger)
         {
             _cartService = cartService;
+            _logger = logger;
         }
         [HttpGet]
+        [ResponseCache(Duration = 30)]
         public async Task<IEnumerable<Cart>> Get()
         {
             return await _cartService.Get();
@@ -26,6 +30,7 @@ namespace API.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult<Cart>> Get(int id)
         {
+            _logger.LogInformation($"Got a cart with id {id}");
             return await _cartService.GetCartByIdAsync(id);
         }
         [HttpPost]
@@ -35,15 +40,18 @@ namespace API.Controllers
 
             _cartService.Create(cart);
 
+            _logger.LogInformation($"Was created new cart!");
+
             return Ok("Successfully created new cart!");
         }
-
         [HttpPut]
         public ActionResult Edit(Cart cart)
         {
             if(cart == null) return BadRequest();
 
             _cartService.Edit(cart);
+
+            _logger.LogInformation($"Was edited cart!");
 
             return Ok("Successfully updated selected cart!");
         }
@@ -54,6 +62,8 @@ namespace API.Controllers
             if(id == 0) return BadRequest();
 
             _cartService.Delete(id);
+
+            _logger.LogInformation($"Was delited cart!");
 
             return Ok("Successfully deleted selected cart");
         }
